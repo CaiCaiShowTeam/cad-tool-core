80ee358d24fd5bf319b217933156f035d9628f7c
Jeason <Jeason@172.16.243.1>
2018-10-07 15:23:56 +0800

    format.Jeason

--- src/priv/lee/cad/ui/Option.java
+++ src/priv/lee/cad/ui/Option.java
@@ -40,11 +40,11 @@ public class Option extends JButton implements ResourceMapper {
 			window.dispose();
 		}
 	}
+	
 	public static final String BROWSE_BUTTON = "browse";
 	public static final String CONFIRM_BUTTON = "confirm";
 	private static final String PREFIX = Option.class.getSimpleName().toLowerCase();
 	protected static ResourceMap resourceMap = new ComponentResourceMap(PREFIX, Option.class);
-
 	private static final long serialVersionUID = -7593230479709858017L;
 
 	public static Option newCancelOption(Window window) {
